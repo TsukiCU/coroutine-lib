32714087e28c7575e11a565d7baa3a300c9fe7da
TsukiCU <hs3475@columbia.edu>
2024-08-09 00:12:06 -0400

    minor

--- fiber_lib/1thread/thread.cpp
+++ fiber_lib/1thread/thread.cpp
@@ -20,12 +20,12 @@ Thread* Thread::GetThis()
     return t_thread;
 }
 
-const std::string& Thread::GetName() 
+const std::string& Thread::GetName()
 {
     return t_thread_name;
 }
 
-void Thread::SetName(const std::string &name) 
+void Thread::SetName(const std::string &name)
 {
     if (t_thread) 
     {
@@ -34,11 +34,11 @@ void Thread::SetName(const std::string &name)
     t_thread_name = name;
 }
 
-Thread::Thread(std::function<void()> cb, const std::string &name): 
-m_cb(cb), m_name(name) 
+Thread::Thread(std::function<void()> cb, const std::string &name):
+m_cb(cb), m_name(name)
 {
     int rt = pthread_create(&m_thread, nullptr, &Thread::run, this);
-    if (rt) 
+    if (rt)
     {
         std::cerr << "pthread_create thread fail, rt=" << rt << " name=" << name;
         throw std::logic_error("pthread_create error");
@@ -47,21 +47,21 @@ m_cb(cb), m_name(name)
     m_semaphore.wait();
 }
 
-Thread::~Thread() 
+Thread::~Thread()
 {
-    if (m_thread) 
+    if (m_thread)
     {
         pthread_detach(m_thread);
         m_thread = 0;
     }
 }
 
-void Thread::join() 
+void Thread::join()
 {
-    if (m_thread) 
+    if (m_thread)
     {
         int rt = pthread_join(m_thread, nullptr);
-        if (rt) 
+        if (rt)
         {
             std::cerr << "pthread_join failed, rt = " << rt << ", name = " << m_name << std::endl;
             throw std::logic_error("pthread_join error");
@@ -70,7 +70,7 @@ void Thread::join()
     }
 }
 
-void* Thread::run(void* arg) 
+void* Thread::run(void* arg)
 {
     Thread* thread = (Thread*)arg;
 
@@ -80,8 +80,8 @@ void* Thread::run(void* arg)
     pthread_setname_np(pthread_self(), thread->m_name.substr(0, 15).c_str());
 
     std::function<void()> cb;
-    cb.swap(thread->m_cb); // swap -> 可以减少m_cb中只能指针的引用计数
-    
+    cb.swap(thread->m_cb); // swap -> 可以减少m_cb中智能指针的引用计数
+
     // 初始化完成
     thread->m_semaphore.signal();
 
@@ -89,5 +89,5 @@ void* Thread::run(void* arg)
     return 0;
 }
 
-} 
+}
 
